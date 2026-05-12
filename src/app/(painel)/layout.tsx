93fde9a745968475f6164b85a50bee397384550a
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-12 01:16:40 -0300

    Layout padrão do grupo de rotas

---
+++ src/app/(painel)/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { Sidebar } from "@/components/layout/sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Estapar Estacionamentos Marcio Moraes",
+  description: "Sistema de gerenciamento de estacionamentos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-br"        
+    >
+      <body className="flex flex-col">
+        <Sidebar />
+        {children}
+      </body>
+    </html>
+  );
+}
