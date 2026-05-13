98155e2ca57ee175619423ce8ac6ab602cb2a280
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-13 14:32:03 -0300

    Form Login - Correções

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -5,10 +5,10 @@ export const LoginForm = () => {
     <div className="bg-white p-7 border border-gray-200 rounded-lg">
         <p className="text-gray-600 mb-6 text-[#394152] font-bold">Entre com suas credenciais para acessar o sistema</p>
         <form action="#" className="flex flex-col">
-        <label htmlFor="email" className="text-[#394152] font-bold mb-1">Email</label>
-        <input type="email" id="email" className="border border-gray-300 rounded-lg p-2 mb-6" />
+        <label htmlFor="username" className="text-[#394152] font-bold mb-1">Usuário:</label>
+        <input type="text" id="username" className="border border-gray-300 rounded-lg p-2 mb-6" />
 
-        <label htmlFor="password" className="text-[#394152] font-bold mb-1">Password</label>
+        <label htmlFor="password" className="text-[#394152] font-bold mb-1">Senha:</label>
         <input type="password" id="password" className="border border-gray-300 rounded-lg p-2 mb-6" />
 
         <button 
