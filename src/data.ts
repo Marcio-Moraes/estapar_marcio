09eea3b50304cf034ab0750cb2c0637fb105ee88
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-13 13:56:08 -0300

    Lista de Dados fictícios

---
+++ src/data.ts
@@ -0,0 +1,93 @@
+export const data = {
+    mensalistas: [
+        {
+            codigo: "000610",
+            nome: "ACYR DE ANDRADE (GMC PARK)",
+            endereco: "RUA JOAQUIM FLORIANO, 620",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 1",
+            acoes: {
+                link: "/garagens/000610",
+                urlCurta: "estapar.app/g000610"
+            }
+        },
+        {
+            codigo: "000615",
+            nome: "ESTACIONAMENTO BERRINI",
+            endereco: "AVENIDA BERRINI, 1200",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 1",
+            acoes: {
+                link: "/garagens/000615",
+                urlCurta: "estapar.app/g000615"
+            }
+        },
+        {
+            codigo: "000620",
+            nome: "PARKING FARIAS",
+            endereco: "RUA DA CONSOLAÇÃO, 150",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 2",
+            acoes: {
+                link: "/garagens/000620",
+                urlCurta: "estapar.app/g000620"
+            }
+        },
+        {
+            codigo: "000625",
+            nome: "MOBI PARK PAULISTA",
+            endereco: "AVENIDA PAULISTA, 2000",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 2",
+            acoes: {
+                link: "/garagens/000625",
+                urlCurta: "estapar.app/g000625"
+            }
+        },
+        {
+            codigo: "000630",
+            nome: "STOP PARK PINHEIROS",
+            endereco: "RUA PINHEIROS, 800",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 3",
+            acoes: {
+                link: "/garagens/000630",
+                urlCurta: "estapar.app/g000630"
+            }
+        },
+        {
+            codigo: "000635",
+            nome: "EASY PARK VILA MARIANA",
+            endereco: "RUA SANTO AMARO, 300",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 3",
+            acoes: {
+                link: "/garagens/000635",
+                urlCurta: "estapar.app/g000635"
+            }
+        },
+        {
+            codigo: "000640",
+            nome: "CITY PARK MOEMA",
+            endereco: "AVENIDA MOEMA, 450",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 4",
+            acoes: {
+                link: "/garagens/000640",
+                urlCurta: "estapar.app/g000640"
+            }
+        },
+        {
+            codigo: "000645",
+            nome: "FAST PARK IPIRANGA",
+            endereco: "RUA SIQUEIRA BUENO, 250",
+            cidadeUf: "SÃO PAULO/SP",
+            regional: "SP 4",
+            acoes: {
+                link: "/garagens/000645",
+                urlCurta: "estapar.app/g000645"
+            }
+        }
+    ]
+
+};
