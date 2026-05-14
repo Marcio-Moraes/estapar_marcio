b22eaf54a33ddc36987b0ca8c0b1b0697e54ff56
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-13 21:33:53 -0300

    ícones User e Logout

--- src/components/layout/barraUserTopo.tsx
+++ src/components/layout/barraUserTopo.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import iconUsuario from "@/assets/ui/icon-usuario.png";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { LuUser } from "react-icons/lu";
 
 export const BarraUserTopo = () => {
   return (
     <div className="flex justify-end w-full mb-4 py-2 px-1">
       <ul className="flex gap-4">
         <li className="flex items-center gap-2 text-sm text-gray-600 font-medium">
-          <Image src={iconUsuario} alt="User" width={18} height={18} />
+          <LuUser />
           Márcio Moraes
         </li>
         <li className="flex items-center gap-2 text-sm text-gray-600 font-medium">
-          <Image src={iconUsuario} alt="User" width={18} height={18} />
+          <FaArrowRightFromBracket />
           Sair
         </li>
       </ul>
